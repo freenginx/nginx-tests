bd233250833a7e63af90b8802970ea5bc5c524cc
Maxim Dounin <mdounin@mdounin.ru>
2024-05-06 00:03:13 +0300

    Tests: relaxed SSL version used in testing.
    This ensures that tests can be properly run with old OpenSSL versions
    when using recent versions of IO::Socket::SSL (which defaults to TLS 1.2+
    starting with IO::Socket:SSL version 2.082, and therefore not compatible
    with OpenSSL before 1.0.1).

--- lib/Test/Nginx.pm
+++ lib/Test/Nginx.pm
@@ -872,6 +872,7 @@ sub http_start($;%) {
 			require IO::Socket::SSL;
 			IO::Socket::SSL->start_SSL(
 				$s,
+				SSL_version => 'SSLv23',
 				SSL_verify_mode =>
 					IO::Socket::SSL::SSL_VERIFY_NONE(),
 				%extra
